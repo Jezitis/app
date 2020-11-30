4425c00080221db04c94053483bd24efde2f03e8
Aleksandr Morozov <a.morozov@webinnovations.ru>
2020-11-30 21:37:46 +0300

    Task-4

---
+++ src/assets/js/products.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    title: 'AirPods Pro',
+    price: 1600,
+    image: '/img/AIRPODS.jpeg',
+  },
+  {
+    title: 'JBL Xtreme',
+    price: 7500,
+    image: '/img/JBL.jpeg',
+  },
+  {
+    title: '2-Х кассетный магнитофон fisher',
+    price: 2500,
+    image: '/img/FISHER.jpeg',
+  },
+  {
+    title: 'Проектор accer X1111 DLP',
+    price: 11000,
+    image: '/img/ACERTV.jpeg',
+  },
+  {
+    title: 'Samsung UE50NU7470U',
+    price: 33990,
+    image: '/img/SAMSUNGTV.jpeg',
+  },
+  {
+    title: 'Hitachi 24HB4T05',
+    price: 5000,
+    image: '/img/HITACHI.jpeg',
+  },
+  {
+    title: 'Ноутбук Lenovo G50-45',
+    price: 7500,
+    image: '/img/LENOVO.jpeg',
+  },
+  {
+    title: 'Acer Aspire V5-552G',
+    price: 1590,
+    image: '/img/ACER.jpeg',
+  },
+  {
+    title: 'Huawei MateBook D 15',
+    price: 44500,
+    image: '/img/HUAVEI.jpeg',
+  },
+];
---
+++ src/components/Products/ProductList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="catalog__list">
+    <product-item
+      v-for="product in products"
+      :key="product.title"
+      :product="product"
+    />
+  </ul>
+</template>
+<script>
+import ProductItem from './ProductItem.vue';
+
+export default {
+  name: 'ProductList',
+  props: {
+    products: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  components: {
+    ProductItem,
+  },
+};
+</script>
